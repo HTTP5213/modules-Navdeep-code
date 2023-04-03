d943421b3dc9d7c7d9eb0af86bdbf61ca9737dd7
Navdeep-kaur1 <59167625+Navdeep-code@users.noreply.github.com>
2023-04-03 09:55:20 -0400

    Update my-module.js

--- public/my-module.js
+++ public/my-module.js
@@ -1,4 +1,3 @@
-
 import { theShire, buckland } from './another-module.js';
 
 const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
